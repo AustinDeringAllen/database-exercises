aac0106d48701ba5bc27c10c7fff848e9b2462fd
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-04 11:03:43 -0500

    Albums table created

---
+++ albums_migration.sql
@@ -0,0 +1,25 @@
+USE codeup_test_db;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(20),
+    album_name VARCHAR(20),
+    release_date INT UNSIGNED,
+    sales DOUBLE,
+    genre VARCHAR(100),
+    PRIMARY KEY (id)
+);
+
+-- id — auto-incrementing unsigned integer primary key
+-- artist — string for storing the recording artist name
+-- name — string for storing a record name
+-- release_date — integer representing year record was released
+-- sales — floating point value for number of records sold (in millions)
+-- genre — string for storing the record's genre(s)
+
+-- CREATE TABLE quotes (
+--     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     author_first_name VARCHAR(50) DEFAULT 'NONE',
+--     author_last_name  VARCHAR(100) NOT NULL,
+--     content TEXT NOT NULL,
+--     PRIMARY KEY (id)
+-- );
