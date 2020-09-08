47e21133bbd69441768a701055c29a4301abdc2f
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-08 12:06:06 -0500

    Second Half of Exercise done

--- where_exercises.sql
+++ where_exercises.sql
@@ -64,3 +64,16 @@ select * from employees where birth_date like ('%12-25');
 
 select * from employees where last_name like ('%q%');
 
+# Second Half
+select * from employees where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+select * from employees where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
+and gender = 'M';
+
+select * from employees where last_name like ('e%') or last_name like ('%e');
+
+select * from employees where last_name like ('e%') and last_name like ('%e');
+
+select * from employees where hire_date like ('199%') and birth_date like ('%12-25');
+
+select * from employees where last_name like ('%q%') and last_name not like ('%qu%');
