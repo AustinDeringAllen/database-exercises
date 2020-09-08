db399b8a6252d7dd575e11d78e16e5fa8a921697
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-08 11:58:52 -0500

    First half of exercise done

---
+++ where_exercises.sql
@@ -0,0 +1,66 @@
+# # CMD Enter to choose what to run
+# USE employees;
+#
+# SELECT * FROM employees WHERE hire_date = '1985-01-01';
+#
+# # Middle of the name; Anywhere in name;
+# select first_name from employees where first_name like '%sus%';
+# # Beginning of name
+# select first_name from employees where first_name like 'sus%';
+# # End of name
+# select first_name from employees where first_name like '%nd';
+#
+# select distinct first_name from employees;
+#
+# select emp_no, first_name, last_name
+# from employees
+# where emp_no between 10026 and 10082;
+#
+# # IN = Includes
+# select emp_no, first_name, last_name
+# from employees
+# where last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+#
+# # Not Null
+# select emp_no, title
+# from titles
+# where to_date is not null;
+#
+# # Chaining Where Clauses
+# select emp_no, first_name, last_name
+# from employees
+# where last_name in ('Herber','Baek')
+#   and emp_no < 20000;
+#
+# # No grouping
+# select emp_no, first_name, last_name
+# from employees
+# where emp_no < 20000
+#     and last_name in ('Herber','Baek')
+#    or first_name = 'Shridhar';
+#
+# # Grouping
+# select emp_no, first_name, last_name
+# from employees
+# where emp_no < 20000
+#   and (
+#             last_name in ('Herber','Baek')
+#         or first_name = 'Shridhar'
+#     );
+#
+# # The first one with no grouping is (emp_no && last_name) || Shridhar
+# # Second one is where emp_no < 20000 && (last_name || first_name
+
+USE employees;
+
+# First Half
+select * from employees where first_name in ('Irena','Vidya','Maya');
+
+select * from employees where first_name like 'e%';
+
+select * from employees where hire_date like ('199%');
+
+select * from employees where birth_date like ('%12-25');
+
+select * from employees where last_name like ('%q%');
+
