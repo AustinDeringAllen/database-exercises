131e8a594bb82fd04a972658b41079b25a7376f2
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-09-09 10:56:09 -0500

    functions_exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -7,3 +7,6 @@ select * from employees where hire_date like ('199%') and birth_date like ('%12-
 order by birth_date asc, hire_date desc;
 
 select datediff()
+
+select concat(first_name,' ',last_name) as 'full_name', datediff(curdate(), hire_date) as 'time_at_company'
+from employees where hire_date like ('199%') and birth_date like ('%12-25');
